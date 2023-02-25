7648f9291994e148eca24cbe20a2ef3d6ebb84ad
Daylight <54731338+Daylight-beyond@users.noreply.github.com>
2023-02-25 17:31:25 +0800

    feat: 测试gh-pages

--- .umirc.ts
+++ .umirc.ts
@@ -9,7 +9,7 @@ if(process.env.SITE_BUILD_ENV === 'PREVIEW') {
 }
 
 export default defineConfig({
-  title: 'HSM DEMO',
+  title: 'HSM UI DEMO',
   mode: 'site',
   outputPath: 'doc-site',
  	exportStatic: {}, // 后续会部署到 github pages 直接全部生成静态页面 不走前端路由
